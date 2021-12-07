fd36f362cdf4019bacef1f6ca9f0802d9a39803e
Piotr Gębala <piotrek.gebala@gmail.com>
2021-12-07 17:57:47 +0100

    Fix issues with  options (#10)

--- spec/opera_spec.rb
+++ spec/opera_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Opera do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Opera::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(true).to eq(true)
   end
 end
